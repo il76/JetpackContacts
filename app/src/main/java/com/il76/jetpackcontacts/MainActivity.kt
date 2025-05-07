1fb2f6200d90fdb4f7d7368027f7551b27310481
il <igurylev@yandex.ru>
2025-05-07 17:28:33 +0300

    Вспомогательные файлы и начало вёрстки

--- app/src/main/java/com/il76/jetpackcontacts/MainActivity.kt
+++ app/src/main/java/com/il76/jetpackcontacts/MainActivity.kt
@@ -4,25 +4,65 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.il76.jetpackcontacts.ui.theme.JetpackContactsTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             JetpackContactsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text(stringResource(R.string.app_name))
+                            }
+                        )
+                    },
+                    modifier = Modifier
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
+                    ) {
+                        ContactDetails(Contact(
+                            name = "Киняев",
+                            surname = "Джейсонович",
+                            familyName = "Киняев",
+                            imageRes = null,
+                            isFavorite = false,
+                            phone = "+7 322 222 33 22",
+                            address = "000, Hollywood Boulevard, Los Angeles, CA 90028.",
+                            email = "info@abc.dev"
+                        ))
+                    }
                 }
             }
         }
@@ -30,16 +70,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ContactDetails(contact: Contact, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (contact.imageRes != null) {
+            Image(
+                painter = painterResource(contact.imageRes),
+                contentDescription = null,
+                modifier = Modifier.size(150.dp)
+            )
+        }
+        Text(
+            text = "${contact.name} ${contact.surname}".trim(),
+            modifier = modifier
+        )
+        Text(
+            text = contact.familyName,
+            modifier = modifier,
+            fontSize = 26.sp
+        )
+    }
+
+
 }
 
+@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ContactDetailsNophotoPreview() {
     JetpackContactsTheme {
-        Greeting("Android")
+        ContactDetails(Contact(
+            name = "Фома",
+            surname = "Джейсонович",
+            familyName = "Киняев",
+            imageRes = null,
+            isFavorite = false,
+            phone = "+7 322 222 33 22",
+            address = "000, Hollywood Boulevard, Los Angeles, CA 90028.",
+            email = "info@abc.dev"
+        ))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ContactDetailsPreview() {
+    JetpackContactsTheme {
+        ContactDetails(Contact(
+            name = "Фома",
+            surname = "Джейсонович",
+            familyName = "Киняев",
+            imageRes = R.drawable.matt_daymon,
+            isFavorite = false,
+            phone = "+7 322 222 33 22",
+            address = "000, Hollywood Boulevard, Los Angeles, CA 90028.",
+            email = "info@abc.dev"
+        ))
     }
 }
